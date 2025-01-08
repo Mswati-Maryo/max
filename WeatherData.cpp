8f314e98f515e86296917cc3cd271b6c7ddc2bbc
Mswati-Maryo <mswatimaryo@gmail.com>
2025-01-08 14:14:44 +0300

    Add files via upload

---
+++ WeatherData.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <limits>
+
+struct WeatherData {
+	std::string date;
+	double temperature;
+};
